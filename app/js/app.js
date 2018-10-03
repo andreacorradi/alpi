3e9e2a9db274f40be6d8e777180dff7dc08af135
andreacorradi <jjcorradi@gmail.com>
2018-10-03 11:13:38 +0200

    triangular mountains

--- app/js/app.js
+++ app/js/app.js
@@ -3,13 +3,23 @@ const output = document.getElementById("demo")
 const main = document.getElementById("main-container")
 const container = document.getElementById("peaks-container")
 const countrySel = document.getElementById("country-sel")
+const orderSel = document.getElementById("order-sel")
 const svg = document.getElementById("peaks-chart")
+
+const angleThreshold = 100
 
 let svgW = svg.clientWidth
 let containerW = container.clientWidth
 let containerH = container.clientHeight
 
+let selCountry = 'All'
+let selOrder = 'height'
+
 let dataLenght
+
+const scaleY = d3.scaleLinear()
+	.domain([0, 4808])
+	.range([0, containerH])
 
 function init() {
 
@@ -35,87 +45,189 @@ function init() {
 
 		const countries = getUniqueValuesOfKey(mountains, 'country')
 		countries.unshift("All")
-		countries.forEach((c) => {
-			const option = document.createElement("option")
-			option.appendChild(document.createTextNode(c))
-			countrySel.appendChild(option)
-		})
+		populateSel(countrySel, countries)
+		const orderPars = ['height', 'lat', 'drop']
+		populateSel(orderSel, orderPars)
 
-		let skyline = d3.select('svg').append("path")
+		// let skyline = d3.select('svg').append('path')
+		// updateLine(selCountry, selOrder)
 
-		update('All')
+		let skyline = d3.select('svg')
+		updateTriangle(selCountry, selOrder)
 
-		function update(filterValue) {
 
-			let selectedMountains = filterSortData(mountains, 'country', filterValue, 'lat')
-			let data = []
-			selectedMountains.forEach((m, i) => {
-				let summit = Object.assign({}, m)
-				summit.side = "summit"
-				data.push(summit)
-				let drop = Object.assign({}, m)
-				drop.side = "drop"
-				data.push(drop)
+		// function updateLine(filterValue, orderValue) {
+
+		// 	let selectedMountains = filterSortData(mountains, 'country', filterValue, orderValue)
+		// 	let data = []
+		// 	selectedMountains.forEach((m, i) => {
+		// 		let summit = Object.assign({}, m)
+		// 		summit.side = "summit"
+		// 		data.push(summit)
+		// 		let drop = Object.assign({}, m)
+		// 		drop.side = "drop"
+		// 		data.push(drop)
+		// 	})
+		// 	console.log(data)
+		// 	dataLenght = data.length
+		// 	let span = svgW / data.length
+
+		// 	const lineGround = d3.line()
+		// 		.x((d, i) => i*span)
+		// 		.y((d, i) => containerH)
+
+		// 	const line = d3.line()
+		//     .x((d, i) => i*span)
+		//     .y((d) => {	if (d.side === "summit") { return containerH - d.height/10 } else { return containerH - (d.height - d.drop)/10 } })
+			
+		// 	zoomBox(container, '0')
+		// 	slider.value = 1
+
+		//   skyline
+		//     .datum(data)
+		//     .attr('d', lineGround)
+		//   	.transition()
+	 //  		.duration(1000)
+		//     .attr('d', line)
+		//     .style('fill', 'none')
+		//     .style('stroke', 'black')
+		// 		.style('stroke-width', 1)
+
+		// 	let points = d3.select('svg').selectAll("circle")
+		//     .data(data)
+
+		//   points
+		//     	.attr("cx", (d, i) => i*span)
+		//     	.attr("cy", containerH)
+		//   	.transition()
+		//   	.duration(1000)
+		// 	    .attr("cy", (d) => {	if (d.side === "summit") { return containerH - d.height/10 } else { return containerH - (d.height - d.drop)/10 } })
+
+		//   let newPoints = points
+		// 	  .enter().append("circle")
+		// 	    .attr("cx", (d, i) => i*span)
+		// 	    .attr("cy", containerH)
+		// 	  	.transition()
+		//   		.duration(1000)
+		// 	    .attr("cy", (d) => {	if (d.side === "summit") { return containerH - d.height/10 } else { return containerH - (d.height - d.drop)/10 } })
+		// 	    .attr("r", (d) => {	if (d.side === "summit") { return 2 } else { return 2 } })
+		// 	    .style('fill', (d) => {	if (d.side === "summit") { return 'black' } else { return 'blue' } })
+
+		// 	points.exit().remove()
+
+		// 	output.innerHTML = slider.value
+		// }
+
+
+
+		function updateTriangle(filterValue, orderValue) {
+
+			let selectedMountains = filterSortData(mountains, 'country', filterValue, orderValue)
+			let temp = null
+			let data = selectedMountains.map((m, i) => {
+				let obj = Object.assign({}, m)
+				if (i !== 0) {
+					obj.previous = {
+						height: temp.height,
+						drop: temp.drop
+					}	
+				}
+				temp = m
+				return obj
 			})
 			console.log(data)
 			dataLenght = data.length
 			let span = svgW / data.length
 
-			const lineGround = d3.line()
-				.x((d, i) => i*span)
-				.y((d, i) => containerH)
+			const maxH = d3.max(data, d => d.height)
 
-			const line = d3.line()
-		    .x((d, i) => i*span)
-		    .y((d) => {	if (d.side === "summit") { return containerH - d.height/10 } else { return containerH - (d.height - d.drop)/10 } })
-			
 			zoomBox(container, '0')
 			slider.value = 1
 
-		  skyline
-		    .datum(data)
-		    .attr('d', lineGround)
-		  	.transition()
-	  		.duration(1000)
-		    .attr('d', line)
-		    .style('fill', 'none')
-		    .style('stroke', 'black')
-				.style('stroke-width', 1)
-
-			let points = d3.select('svg').selectAll("circle")
+			let triangles = d3.select('svg').selectAll('polygon')
 		    .data(data)
 
-		  points
-		    	.attr("cx", (d, i) => i*span)
-		    	.attr("cy", containerH)
+		  triangles
+	    	.attr('points', (d, i) => calcPoints(d, i, 0))
 		  	.transition()
 		  	.duration(1000)
-			    .attr("cy", (d) => {	if (d.side === "summit") { return containerH - d.height/10 } else { return containerH - (d.height - d.drop)/10 } })
+	  		.attr('points', (d, i) => calcPoints(d, i, 1))
 
-		  let newPoints = points
-			  .enter().append("circle")
-			    .attr("cx", (d, i) => i*span)
-			    .attr("cy", containerH)
+		  let newTriangles = triangles
+			  .enter().append('polygon')
+			  	.attr('points', (d, i) => calcPoints(d, i, 0))
+			    .style('fill', 'blue')
+			    .style('fill-opacity', 0.1)
+			    .style('stroke', 'none')
 			  	.transition()
 		  		.duration(1000)
-			    .attr("cy", (d) => {	if (d.side === "summit") { return containerH - d.height/10 } else { return containerH - (d.height - d.drop)/10 } })
-			    .attr("r", 2)
-			    .style('fill', (d) => {	if (d.side === "summit") { return 'blue' } else { return "black" } })
+			  	.attr('points', (d, i) => calcPoints(d, i, 1))
 
-			points.exit().remove()
+			triangles.exit().remove()
+
+
+			function calcPoints(d, i, step) {
+				let H = 0
+				step === 1 ? H = scaleY(d.height) : H = 0
+				let x_sub = 0
+  			const base = span/2
+  			const altezza = d.drop
+  			const x_add = (base * (d.height - altezza))/altezza
+  			if (i !== 0) {
+	  			const altezza_b = d.previous.drop - (d.previous.height - d.height)
+	  			if (altezza_b > angleThreshold) {
+	  				x_sub = (base * (d.previous.height - d.previous.drop))/altezza_b
+	  			} else x_sub = 0
+  			}
+  			const points = (i*span - span/2 - x_sub) + ',' + containerH + ' ' + i*span + ',' + (containerH - H) + ' ' + (i*span + span/2 + x_add) + ',' + containerH
+  			return points
+			}
+
+			// let points = d3.select('svg').selectAll("circle")
+		 //    .data(data)
+
+		 //  points
+		 //    	.attr("cx", (d, i) => i*span)
+		 //    	.attr("cy", containerH)
+		 //  	.transition()
+		 //  	.duration(1000)
+			//     .attr("cy", (d) => {	if (d.side === "summit") { return containerH - scaleY(d.height) } else { return containerH - scaleY(d.height - d.drop) } })
+
+		 //  let newPoints = points
+			//   .enter().append("circle")
+			//     .attr("cx", (d, i) => (i*span) + span/2)
+			//     .attr("cy", containerH)
+			//   	.transition()
+		 //  		.duration(1000)
+			//     .attr("cy", (d) => containerH - scaleY(d.height - d.drop))
+			//     .attr("r", 2)
+			//     .style('fill', 'black')
+
+			// points.exit().remove()
 
 			output.innerHTML = slider.value
 		}
 
+
+
+
 		slider.oninput = function(e) {
 		  output.innerHTML = e.target.value
 		  zoomBox(container, e.target.value)
-		  zoomSvg(svg, containerW, dataLenght)
+		  //zoomSvgLine(svg, containerW, dataLenght)
+		  zoomSvgTriangle(svg, containerW, dataLenght)
 		}
 
 		countrySel.onchange = function(e) {
-			const selCountry = e.target.options[e.target.selectedIndex].value
-			update(selCountry)
+			selCountry = e.target.options[e.target.selectedIndex].value
+			//updateLine(selCountry, selOrder)
+			updateTriangle(selCountry, selOrder)
+		}
+
+		orderSel.onchange = function(e) {
+			selOrder = e.target.options[e.target.selectedIndex].value
+			//updateLine(selCountry, selOrder)
+			updateTriangle(selCountry, selOrder)
 		}
 
 	})
@@ -133,7 +245,7 @@ function zoomBox(container, magnification) {
   main.scrollLeft = offsetW
 }
 
-function zoomSvg(svg, targetW, dataLength) {
+function zoomSvgLine(svg, targetW, dataLength) {
 	svg.clientWidth = targetW
 	const newSpan = targetW / dataLength
 	const lineGround = d3.line()
@@ -149,13 +261,37 @@ function zoomSvg(svg, targetW, dataLength) {
 		.attr("cx", (d, i) => i*newSpan)
 }
 
+function zoomSvgTriangle(svg, targetW, dataLength) {
+	svg.clientWidth = targetW
+	const newSpan = targetW / dataLength
+	function calcPoints(d, i, step) {
+		let H = 0
+		step === 1 ? H = scaleY(d.height) : H = 0
+		let x_sub = 0
+		const base = newSpan/2
+		const altezza = d.drop
+		const x_add = (base * (d.height - altezza))/altezza
+		if (i !== 0) {
+			const altezza_b = d.previous.drop - (d.previous.height - d.height)
+			if (altezza_b > angleThreshold) {
+				x_sub = (base * (d.previous.height - d.previous.drop))/altezza_b
+			} else x_sub = 0
+		}
+		const points = (i*newSpan - newSpan/2 - x_sub) + ',' + containerH + ' ' + i*newSpan + ',' + (containerH - H) + ' ' + (i*newSpan + newSpan/2 + x_add) + ',' + containerH
+		return points
+	}
+	d3.selectAll('polygon')
+		.attr('points', (d, i) => calcPoints(d, i, 0))
+		.attr('points', (d, i) => calcPoints(d, i, 1))
+}
+
 function filterSortData(data, filterField, filterValue, sortField) {
 	return data
 		.filter((d) => {
 			if (filterValue === 'All') return d
 			else return d[filterField] === filterValue
 		})
-		.sort((a,b) => a[sortField] - b[sortField])
+		.sort((a,b) => b[sortField] - a[sortField])
 }
 
 function getUniqueValuesOfKey(array, key){
@@ -163,6 +299,14 @@ function getUniqueValuesOfKey(array, key){
     if(item[key] && !~carry.indexOf(item[key])) carry.push(item[key])
     return carry
   }, [])
+}
+
+function populateSel(select, arr) {
+	arr.forEach((c) => {
+		const option = document.createElement("option")
+		option.appendChild(document.createTextNode(c))
+		select.appendChild(option)
+	})
 }
 
 // $(window).on('resize', () => {
